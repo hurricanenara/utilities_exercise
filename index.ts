49476c77da1acf5f834b2882fdb6fb0f0f4d31b6
rsl150101 <dnwnd501@naver.com>
2023-02-01 05:22:18 +0900

    문제 다 품

--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ interface Member extends User {
     myProfile객체에 존재하는 모든 프로퍼티 필수
 */
 
-const myProfile: Member = {
+const myProfile: Partial<Member> = {
   username: "dobby",
   imgUrl: "",
   lastVisited: "1675085621467",
@@ -31,14 +31,21 @@ const myProfile: Member = {
     2. dobbyFollowers와 harryFollowers가 컴파일 되도록 하세요.
 */
 
-const dobbyFollowers: Member = [{ usrename: "harry", imgUrl: "harry.jpg" }];
-const harryFollowers: Member = [];
+const dobbyFollowers: Pick<Member, "followers">["followers"] = [
+  { username: "harry", imgUrl: "harry.jpg" },
+];
+const harryFollowers: Partial<Member>[] = [];
 
 /*
     3. http 모듈에서 ClientRequestArgs를 import하고 host, headers, 그리고 method를 분리하세요.
 */
 
-type ClientRequestHeadersAndHost = {};
+import { ClientRequestArgs } from "http";
+
+type ClientRequestHeadersAndHost = Pick<
+  ClientRequestArgs,
+  "host" | "headers" | "method"
+>;
 
 const clientRequest: ClientRequestHeadersAndHost = {
   method: "GET",
@@ -51,33 +58,81 @@ const clientRequest: ClientRequestHeadersAndHost = {
     전환한 후 mySocketOptions을 완성하세요.
 */
 
-type NoOptionalTLSSocketOptions = {};
+import { TLSSocketOptions } from "tls";
+const net = require("net");
+
+type NoOptionalTLSSocketOptions = Required<TLSSocketOptions>;
 
 const mySocketOptions: NoOptionalTLSSocketOptions = {
   isServer: true,
-  // ...
+  server: net.Server,
+  session: Buffer.from(""),
+  requestOCSP: true,
+  ca: "string | Buffer | (string | Buffer)[]",
+  cert: "string | Buffer | Array<string | Buffer> | undefined",
+  sigalgs: "string | undefined",
+  ciphers: "string | undefined",
+  clientCertEngine: "string | undefined",
+  crl: "string | Buffer | Array<string | Buffer> | undefined",
+  dhparam: "string | Buffer | undefined",
+  ecdhCurve: "string | undefined",
+  honorCipherOrder: true,
+  key: "string | Buffer | Array<string | Buffer | KeyObject> | undefined",
+  privateKeyEngine: "string | undefined",
+  privateKeyIdentifier: "string | undefined",
+  maxVersion: "TLSv1.3",
+  minVersion: "TLSv1.3",
+  passphrase: "string | undefined",
+  pfx: "string | Buffer | Array<string | Buffer | PxfObject> | undefined",
+  secureOptions: 123, // Value is a numeric bitmask of the `SSL_OP_*` options
+  secureProtocol: "string | undefined",
+  sessionIdContext: "string | undefined",
+  ticketKeys: Buffer.from(""),
+  sessionTimeout: 123,
+  secureContext: net,
+  enableTrace: true,
+  requestCert: true,
+  ALPNProtocols: [],
+  SNICallback: net,
+  rejectUnauthorized: true,
 };
 
 /*
     5. net 모듈에서 SocketReadyState을 import하고 다음을 완성하세요. 새로운 타입 선언 금지.
 */
 
-const openState: Type = "open";
-const readOnlyState: Type = "readOnly";
-const closedState: Type = "closed";
+import { SocketReadyState } from "net";
+
+const openState: Extract<SocketReadyState, "open"> = "open";
+const readOnlyState: Extract<SocketReadyState, "readOnly"> = "readOnly";
+const closedState: Extract<SocketReadyState, "closed"> = "closed";
 
 /*
     6. SocketReadyState를 사용해서 다음을 충족하는 타입을 완성하고 반영하세요.
 */
 
-const allStatesExceptClosed = ["opening", "open", "readOnly", "writeOnly"]; // ✅
-const iShouldntCompile = ["opening", "open", "readOnly", "writeOnly", "closed"]; // ❌
+const allStatesExceptClosed: Exclude<SocketReadyState, "closed">[] = [
+  "opening",
+  "open",
+  "readOnly",
+  "writeOnly",
+]; // ✅
+const iShouldntCompile: Exclude<SocketReadyState, "closed">[] = [
+  "opening",
+  "open",
+  "readOnly",
+  "writeOnly",
+  // "closed",
+]; // ❌
 
 /*
     7. 6번에서 완성 한 타입으로 다음을 충족하는 타입을 만드세요.
 */
 
-const states = {
+const states: Record<
+  Exclude<SocketReadyState, "closed">,
+  Exclude<SocketReadyState, "closed">
+> = {
   opening: "opening",
   open: "open",
   readOnly: "readOnly",
@@ -90,39 +145,60 @@ const states = {
     그런 후, MemoryUsage의 프로퍼티 중 2개를 사용해보세요.
 */
 
-const memoryUsage = {};
+// import { MemoryUsage } from "process";
+
+interface MemoryUsage {
+  rss: number;
+  heapTotal: number;
+  heapUsed: number;
+  external: number;
+  arrayBuffers: number;
+}
+
+const memoryUsage: Partial<MemoryUsage> = {
+  rss: 123,
+  heapTotal: 456,
+  // heapUsed: number,
+  // external: number,
+  // arrayBuffers: number
+};
 
 /*
     9. null과 undefined가 있는 현실적인 타입을 만들어보고, 만들어진 타입을 non-nullable 타입으로 전환하세요.
 */
 
-type CustomType = {};
+type CustomType = {
+  wallet: null;
+  alien: undefined;
+};
+
+type NonNullableCustomType = NonNullable<CustomType>;
 
 /*
     10. 다음 함수들의 반환 타입을 유틸리티 타입을 사용해 정의해보세요.
 */
 
-function getsUsername(user: User) {
+function getsUsername(user: User): TGetUserNameReturn {
   return user.username;
 }
 
-type TGetUserNameReturn = {};
+type TGetUserNameReturn = Pick<User, "username">["username"];
 
 // ---
 
-function getUser(member: Member) {
+function getUser(member: Member): TGetUserReturn {
   return { username: member.username, imgUrl: member.imgUrl } as User;
 }
 
-type TGetUserReturn = {};
+type TGetUserReturn = Pick<Member, "username" | "imgUrl">;
 
 // ---
 
-function getFollowersAndViewCount(member: Member) {
+function getFollowersAndViewCount(member: Member): TFollowerAndViewCountReturn {
   return {
     followers: member.followers,
     viewCount: member.viewCount,
   };
 }
 
-type TFollowerAndViewCountReturn = {};
+type TFollowerAndViewCountReturn = Pick<Member, "followers" | "viewCount">;
